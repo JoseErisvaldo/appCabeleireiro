6c9429555cd2c74667808725df9d78f62613a13c
José Santos <100242848+JoseErisvaldo@users.noreply.github.com>
2024-06-02 23:57:18 +0000

    Feat: Link Cards

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -9,11 +9,11 @@ export default function Home () {
     return(
         <div>
             <div>Olá, José Erisvaldo ! :D</div>
-            <Card iconLogo={<FaRegCalendarAlt />} title={'Realizar Agendamentos'} iconArrow={<FaArrowRight />} />
-            <Card iconLogo={<FaRegCalendarAlt />} title={'Consutar Agendamentos'} iconArrow={<FaArrowRight />} />
-            <Card iconLogo={<FaMoneyCheckDollar />} title={'Pagamentos Pendentes'} iconArrow={<FaArrowRight />} />
-            <Card iconLogo={<FaMoneyCheckDollar />} title={'Pagamentos Aprovados'} iconArrow={<FaArrowRight />} />
-            <Card iconLogo={<MdOutlineSupervisorAccount />} title={'Clientes'} iconArrow={<FaArrowRight />} />
+            <Card to={'realizaragendamentos'} iconLogo={<FaRegCalendarAlt />} title={'Realizar Agendamentos'} iconArrow={<FaArrowRight />} />
+            <Card to={'consultaragendamentos'} iconLogo={<FaRegCalendarAlt />} title={'Consutar Agendamentos'} iconArrow={<FaArrowRight />} />
+            <Card to={'pagamentosaprovados'} iconLogo={<FaMoneyCheckDollar />} title={'Pagamentos Pendentes'} iconArrow={<FaArrowRight />} />
+            <Card to={'pagamentospendentes'} iconLogo={<FaMoneyCheckDollar />} title={'Pagamentos Aprovados'} iconArrow={<FaArrowRight />} />
+            <Card to={'clientes'} iconLogo={<MdOutlineSupervisorAccount />} title={'Clientes'} iconArrow={<FaArrowRight />} />
             
         </div>
     )
